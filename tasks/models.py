24dc4332976bf9051c5730256deee13b6efb3230
Julian Gamba <gahe2003@gmail.com>
2024-05-28 13:15:33 -0500

    se modifica el modelo

--- tasks/models.py
+++ tasks/models.py
@@ -2,26 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Task(models.Model):
-
+    
+    BACKLOG = 'BACKLOG'
     TO_DO = 'TO DO'
     DOING = 'DOING'
+    TEST = 'TEST'
     DONE = 'DONE'
-    BACKLOG = 'BACKLOG'
-    USER_TEST = 'USER TEST'
-    TESTING = 'TESTING'
 
     STATE_CHOICES = [
+        (BACKLOG, 'Backlog'),
         (TO_DO, 'To do'),
         (DOING, 'Doing'),
+        (TEST, 'Test'),
         (DONE, 'Done'),
-        (BACKLOG, 'Backlog'),
-        (USER_TEST, 'User test'),
-        (TESTING, 'Testing')
     ]
 
     name = models.CharField(max_length=40, verbose_name='Nombre de la tarea')
     description = models.CharField(max_length=255, verbose_name='Descripción de la tarea')
     state = models.CharField(max_length=30, choices=STATE_CHOICES, verbose_name='Estado de la tarea')
+    priority = models.CharField(max_length=30)
     date = models.DateField(verbose_name='Fecha de la tarea')
     owner = models.ForeignKey('auth.User', related_name='tasks', on_delete=models.CASCADE, verbose_name='Dueño de la tarea')
 
