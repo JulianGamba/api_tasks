cdfed96fb389c13039bb4e2304a7b4d1fa1d44fd
Julian Gamba <gahe2003@gmail.com>
2024-05-31 16:28:49 -0500

    Se agregan filtros para el deadline

--- tasks/filters.py
+++ tasks/filters.py
@@ -7,7 +7,10 @@ class TaskFilter(filters.FilterSet):
     state_name = filters.CharFilter(field_name='state__name', lookup_expr='iexact')
     priority_id = filters.NumberFilter(field_name='priority_id', lookup_expr='exact')
     priority_name = filters.CharFilter(field_name='priority__name', lookup_expr='iexact')
+    deadline = filters.DateFilter(field_name='deadline', lookup_expr='exact')
+    deadline_before = filters.DateFilter(field_name='deadline', lookup_expr='lte')
+    deadline_after = filters.DateFilter(field_name='deadline', lookup_expr='gte')
 
     class Meta:
         model = Task
-        fields = ['state_id', 'state_name', 'priority_id', 'priority_name']
+        fields = ['state_id', 'state_name', 'priority_id', 'priority_name', 'deadline', 'deadline_before', 'deadline_after']
