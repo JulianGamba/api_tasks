0ed94d082ab73f771ad53576484eb9345bce6310
Lubin Stiven <lubinstevenr@gmail.com>
2024-05-31 21:12:37 -0500

    se creo la vista basada en clases para permitir listar todos los uduarios y crear nuevos usuarios

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,13 @@
 from rest_framework import viewsets
-from .models import State, Priority, Task
+from .models import State, Priority, Task, CustomUser
 from .serializers import StateSerializer, PrioritySerializer, TaskSerializer, UserSerializer
 from django.contrib.auth.models import User
 
 
-from rest_framework.decorators import api_view
+from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework import status
-from .forms import SignUpForm
+from .serializers import CustomUserSerializer
+
 
 class StateViewSet(viewsets.ModelViewSet):
     queryset = State.objects.all()
@@ -25,13 +25,7 @@ class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     
-    
-@api_view(['POST'])
-def signup(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.data)
-        if form.is_valid():
-            user = form.save()
-            return Response({'message': 'Usuario registrado exitosamente'}, status=status.HTTP_201_CREATED)
-        else:
-            return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+ 
+class CustomUserListCreateAPIView(generics.ListCreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
