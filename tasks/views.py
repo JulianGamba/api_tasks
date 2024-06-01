d9304ec0f969bb18177fe26dfb273efeb078c9ff
Lubin Stiven <lubinstevenr@gmail.com>
2024-05-31 23:36:08 -0500

    se crearon las vistas para el registro e inicio de session de los user

--- tasks/views.py
+++ tasks/views.py
@@ -1,32 +1,47 @@
 from rest_framework import viewsets
-from .models import State, Priority, Task, CustomUser
-from .serializers import StateSerializer, PrioritySerializer, TaskSerializer, UserSerializer
-from django.contrib.auth.models import User
-
-
 from rest_framework import generics, status
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import CustomUserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from .models import State, Priority, Task, CustomUser
+from .serializers import StateSerializer, PrioritySerializer, TaskSerializer, UserSerializer, CustomUserSerializer, LoginSerializer
 
 
 class StateViewSet(viewsets.ModelViewSet):
     queryset = State.objects.all()
     serializer_class = StateSerializer
 
+
 class PriorityViewSet(viewsets.ModelViewSet):
     queryset = Priority.objects.all()
     serializer_class = PrioritySerializer
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    
- 
-class CustomUserListCreateAPIView(generics.ListCreateAPIView):
+
+
+class RegisterAPIView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
-    
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }, status=status.HTTP_200_OK)
