36406b3a2159e8e42a124aa949dcfcf71c03cf99
Lubin Stiven <lubinstevenr@gmail.com>
2024-05-31 23:27:58 -0500

    se creo los serializadores para el registro e inicio de session

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,21 +1,28 @@
 from rest_framework import serializers
 from .models import Task, Priority, State, CustomUser
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+
 
 class StateSerializer(serializers.ModelSerializer):
     class Meta:
         model = State
         fields = '__all__'
 
+
 class PrioritySerializer(serializers.ModelSerializer):
     class Meta:
         model = Priority
         fields = '__all__'
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
+
 
 class TaskSerializer(serializers.ModelSerializer):
     state = serializers.PrimaryKeyRelatedField(queryset=State.objects.all())
@@ -26,8 +33,42 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
-        
-    class CustomUserSerializer(serializers.ModelSerializer):
-        class Meta:
-         model = CustomUser
-        fields = ('id', 'username', 'full_name', 'avatar', 'birth_date', 'identification')
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'password', 'email', 'full_name', 'avatar', 'birth_date', 'identification')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data.get('email', ''),
+            full_name=validated_data.get('full_name', ''),
+            avatar=validated_data.get('avatar', None),
+            birth_date=validated_data.get('birth_date', None),
+            identification=validated_data.get('identification', None),
+        )
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user:
+                data['user'] = user
+            else:
+                raise serializers.ValidationError("Invalid username or password.")
+        else:
+            raise serializers.ValidationError("Must include 'username' and 'password'.")
+
+        return data
