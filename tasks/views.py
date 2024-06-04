3cb3eb32d3c80322babb5744c48cdb40dda5e9db
Julian Gamba <gahe2003@gmail.com>
2024-06-04 18:21:02 -0500

    se elimina vista de task con viewset

--- tasks/views.py
+++ tasks/views.py
@@ -10,12 +10,10 @@ from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import TaskFilter
 
-
 class StateViewSet(viewsets.ModelViewSet):
     queryset = State.objects.all()
     serializer_class = StateSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-
 
 class PriorityViewSet(viewsets.ModelViewSet):
     queryset = Priority.objects.all()
@@ -26,21 +24,6 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-
-class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
-    filterset_class = TaskFilter
-    filter_backends = [DjangoFilterBackend]
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
-    def get_serializer_class(self):
-        if self.action in ['list', 'retrieve']:
-            return TaskReadSerializer
-        return TaskWriteSerializer
-    
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
 
 @api_view(['GET', 'POST'])
 def task_list_create(request):
