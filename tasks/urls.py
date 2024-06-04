8c42f7b0c0fd15ef93258fceaf555d4350217733
Julian Gamba <gahe2003@gmail.com>
2024-06-04 17:57:47 -0500

    Se crean vistas usando decorador api_view

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from .views import task_list_create, tasks_detail
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -10,10 +11,12 @@ router = DefaultRouter()
 router.register(r'states', views.StateViewSet, basename='state')
 router.register(r'prioritys', views.PriorityViewSet, basename='priority')
 router.register(r'users', views.UserViewSet, basename='user')
-router.register(r'tasks', views.TaskViewSet, basename='task')
+# router.register(r'tasks', views.TaskViewSet, basename='task')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('tasks/', task_list_create, name='task-list-create'),
+    path('tasks/<int:pk>/', tasks_detail, name='task-detail'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -42,7 +42,37 @@ class TaskViewSet(viewsets.ModelViewSet):
         serializer.save(owner=self.request.user)
 
 
-# @api_view(['GET', 'POST'])
-# def task_list_create(request):
-#     if request.method == 'GET':
-#         tasks = Task.objects.all()
+@api_view(['GET', 'POST'])
+def task_list_create(request):
+    if request.method == 'GET':
+        tasks = Task.objects.all()
+        serializer = TaskReadSerializer(tasks, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = TaskWriteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def tasks_detail(request, pk, format=None):
+    try:
+        task = Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = TaskReadSerializer(task)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = TaskWriteSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
