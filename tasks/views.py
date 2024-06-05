fa2af46d889d1108348a713f4848ff639083f2c4
Julian Gamba <gahe2003@gmail.com>
2024-06-05 15:59:37 -0500

    Se resuelven conflictos, se unifica y reestructura las vistas

--- tasks/views.py
+++ tasks/views.py
@@ -3,7 +3,6 @@ from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth.models import User
 from .models import State, Priority, Task, CustomUser
 from .serializers import StateSerializer, PrioritySerializer, TaskReadSerializer, TaskWriteSerializer, LoginSerializer, CustomUserSerializer
 from .permissions import IsAuthenticatedOrReadOnly
