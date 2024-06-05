7dbcf172d6c954e1c3d7701f6b5d0730f63ec56d
Lubin Stiven <lubinstevenr@gmail.com>
2024-06-05 17:16:31 -0500

    se movieron los modelos a una carpeta llamada models.py y se creo un archivo para cada modelo

--- WorkStream/models/tasks.py
+++ WorkStream/models/tasks.py
@@ -1,30 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.contrib.auth.models import AbstractUser
 from core import settings
+from django.contrib.auth.models import User
+from .state import State
+from .priority import Priority
 
-
-class State(models.Model):
-    name = models.CharField(max_length=30, verbose_name='Nombre del estado')
-
-    def __str__(self):
-        return self.name
-
-class Priority(models.Model):
-    name = models.CharField(max_length=30, verbose_name='Nombre de la prioridad')
-
-    def __str__(self):
-        return self.name
-    
-class CustomUser(AbstractUser):
-    full_name = models.CharField(max_length=100, blank=True, verbose_name='Nombre completo')
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True, verbose_name='Avatar')
-    birth_date = models.DateField(null=True, blank=True, verbose_name='Fecha de nacimiento')
-    identification =models.PositiveIntegerField(null=True, blank=True, verbose_name='numero de identificacion')
-    def __str__(self):
-        return self.username
 
 class Task(models.Model):
+    
     name = models.CharField(max_length=40, verbose_name='Nombre de la tarea')
     description = models.CharField(max_length=255, verbose_name='Descripción de la tarea')
     state = models.ForeignKey(State, on_delete=models.CASCADE, verbose_name='Estado de la tarea')
@@ -38,8 +20,8 @@ class Task(models.Model):
         return f"tarea: {self.name} en estado {self.state}"
     
     class Meta:
+        
         verbose_name = 'Tarea'
         verbose_name_plural = 'Tareas'
         ordering = ['deadline']
         
-    
