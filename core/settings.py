afa4945831314f8bfb0ddb4e5dfb67b8154e32d7
Lubin Stiven <lubinstevenr@gmail.com>
2024-06-05 19:00:21 -0500

    se configuro la identacion del archivo settings.py

--- core/settings.py
+++ core/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'WorkStream.app.WorkStreamConfig',
+    'WorkStream',
     'django_filters',
 ]
 AUTH_USER_MODEL = 'WorkStream.CustomUser'  
