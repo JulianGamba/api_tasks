79fb5660baeec6760bd6202699042ccdb8bd1d86
Lubin Stiven Reyes Paipilla <lubinstevenr@gmail.com>
2024-06-06 09:50:26 -0500

    se registro el modelo CustomUser en admin.py

--- WorkStream/admin.py
+++ WorkStream/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Task, Priority, State
+from .models.customUser import CustomUser
+
 
 admin.site.register(Task)
 admin.site.register(Priority)
 admin.site.register(State)
+admin.site.register(CustomUser)
