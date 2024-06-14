634d0aa536e9f41c6d0f90adfdf28d49393f7b0f
Lubin Stiven <lubinstevenr@gmail.com>
2024-06-14 08:53:18 -0500

    se ajusto el serializador de comment

--- WorkStream/serializers/commentSerializers.py
+++ WorkStream/serializers/commentSerializers.py
@@ -2,8 +2,9 @@ from rest_framework import serializers
 from WorkStream.models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')  # Solo lectura, se establece automáticamente
-
+    
     class Meta:
+        
         model = Comment
-        fields = ['id', 'user', 'text', 'created_at']
+        fields = ['id', 'task', 'user', 'content', 'created_at', 'updated_at']
+        read_only_fields = ['user', 'created_at', 'updated_at']
