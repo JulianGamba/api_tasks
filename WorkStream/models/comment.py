f8cee096ae79b19f78f7ef3d8cb19e650acd668f
Lubin Stiven <lubinstevenr@gmail.com>
2024-06-14 08:34:34 -0500

    se contruyo la relacion entre los modelos User, Task con el modelo comment

--- WorkStream/models/comment.py
+++ WorkStream/models/comment.py
@@ -1,10 +1,15 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth import get_user_model
+from WorkStream.models import Task  
+
+User = get_user_model()
 
 class Comment(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
+    Task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
+    User = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
-        return self.text[:20]
+        return self.content[:20]
