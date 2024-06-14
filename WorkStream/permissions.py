8d8e494f012833b4fcfb5071f07d9c78b1db46c8
Lubin Stiven <lubinstevenr@gmail.com>
2024-06-14 09:14:17 -0500

    se construyo el metodo para dar los permisos al dueño de la tarea

--- WorkStream/permissions.py
+++ WorkStream/permissions.py
@@ -15,3 +15,16 @@ class IsAuthenticatedOrReadOnly(permissions.BasePermission):
 
         # Write permissions are only allowed to authenticated users.
         return request.user and request.user.is_authenticated
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    Read permissions are allowed to any request,
+    so we'll always allow GET, HEAD or OPTIONS requests.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
