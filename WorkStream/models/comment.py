382e8d79e978314851c3a915d933fe86c8d0b54d
Lubin Stiven <lubinstevenr@gmail.com>
2024-06-17 22:28:05 -0500

    se modifico los datos las relaciones en el modelo

--- WorkStream/models/comment.py
+++ WorkStream/models/comment.py
@@ -5,8 +5,8 @@ from WorkStream.models import Task
 User = get_user_model()
 
 class Comment(models.Model):
-    Task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
-    User = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
