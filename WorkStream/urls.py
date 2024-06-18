5a5704757fd17da2f83f78ab0623be3651d34614
Julian Gamba <gahe2003@gmail.com>
2024-06-18 16:56:11 -0500

    Se agregan urls para acceder a documentación

--- WorkStream/urls.py
+++ WorkStream/urls.py
@@ -1,7 +1,28 @@
 from django.urls import path
 from WorkStream.views import *
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Trello Api",
+      default_version='v1',
+      description="Api simulando el funcionamiento del trello",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
     path('states/', StateViewSet.as_view({'get': 'list', 'post': 'create'}), name='state-list'),
     path('states/<int:pk>/', StateViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='state-detail'),
     path('priorities/', PriorityViewSet.as_view({'get': 'list', 'post': 'create'}), name='priority-list'),
