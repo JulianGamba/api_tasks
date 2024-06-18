8896a48ffcf1d259e7023fb2c4e42a331f42f61a
Lubin Stiven Reyes Paipilla <lubinstevenr@gmail.com>
2024-06-18 16:31:46 -0500

    se agrego la logica para poder asignar cometarios a varios usuarios

--- WorkStream/serializers/commentSerializers.py
+++ WorkStream/serializers/commentSerializers.py
@@ -1,10 +1,27 @@
 from rest_framework import serializers
-from WorkStream.models import Comment
+from WorkStream.models import Comment, Task, CustomUser
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class CommentSerializer(serializers.ModelSerializer):
-    
+    task = serializers.PrimaryKeyRelatedField(queryset=Task.objects.all())
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    assigned_users = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all(), required=False)
+
     class Meta:
-        
         model = Comment
-        fields = ['id', 'task', 'user', 'content', 'created_at', 'updated_at']
-        read_only_fields = ['created_at', 'updated_at']
+        fields = ['id', 'task', 'user', 'assigned_users', 'text', 'created_at']
+        read_only_fields = ['id', 'user', 'created_at']
+
+    def create(self, validated_data):
+        assigned_users = validated_data.pop('assigned_users', [])
+        comment = Comment.objects.create(**validated_data)
+        comment.assigned_users.set(assigned_users)
+        return comment
+
+    def update(self, instance, validated_data):
+        assigned_users = validated_data.pop('assigned_users', [])
+        comment = super().update(instance, validated_data)
+        comment.assigned_users.set(assigned_users)
+        return comment
