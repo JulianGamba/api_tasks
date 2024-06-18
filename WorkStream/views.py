c4c5be33b7f99136c0b80a7bd55de14a55ae4d2b
Julian Gamba <gahe2003@gmail.com>
2024-06-18 16:56:37 -0500

    Se agrega documentación de vistas de estado, prioridad y tarea

--- WorkStream/views.py
+++ WorkStream/views.py
@@ -4,13 +4,42 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Task, State, Priority, CustomUser, Comment
 from .serializers import StateSerializer, PrioritySerializer, CustomUserSerializer, TaskWriteSerializer, TaskReadSerializer, LoginSerializer, CommentSerializer
-
 from .permissions import IsAuthenticatedOrReadOnly, IsCommentOwner, IsOwnerOrAssignedUser
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from django.db.models import Q
 
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_description="Obtiene una lista de todos los estados.",
+    responses={200: StateSerializer(many=True)}
+))
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(
+    operation_description="Obtiene un estado específico por ID.",
+    responses={200: StateSerializer, 404: 'Not Found'}
+))
+@method_decorator(name='create', decorator=swagger_auto_schema(
+    operation_description="Crea un nuevo estado. Acepta múltiples estados si se envía una lista.",
+    request_body=StateSerializer(many=True),
+    responses={201: StateSerializer(many=True)},
+))
+@method_decorator(name='update', decorator=swagger_auto_schema(
+    operation_description="Actualiza un estado específico por ID.",
+    request_body=StateSerializer,
+    responses={200: StateSerializer, 400: 'Bad Request', 404: 'Not Found'}
+))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(
+    operation_description="Actualiza parcialmente un estado específico por ID.",
+    request_body=StateSerializer,
+    responses={200: StateSerializer, 400: 'Bad Request', 404: 'Not Found'}
+))
+@method_decorator(name='destroy', decorator=swagger_auto_schema(
+    operation_description="Elimina un estado específico por ID.",
+    responses={204: 'No Content', 404: 'Not Found'}
+))
 class StateViewSet(viewsets.ModelViewSet):
     
     queryset = State.objects.all()
@@ -28,7 +57,33 @@ class StateViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save()
 
-
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_description="Obtiene una lista de todas las prioridades.",
+    responses={200: PrioritySerializer(many=True)}
+))
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(
+    operation_description="Obtiene una prioridad específica por ID.",
+    responses={200: PrioritySerializer, 404: 'Not Found'}
+))
+@method_decorator(name='create', decorator=swagger_auto_schema(
+    operation_description="Crea una nueva prioridad. Acepta múltiples prioridades si se envía una lista.",
+    request_body=PrioritySerializer(many=True),
+    responses={201: PrioritySerializer(many=True)},
+))
+@method_decorator(name='update', decorator=swagger_auto_schema(
+    operation_description="Actualiza una prioridad específica por ID.",
+    request_body=PrioritySerializer,
+    responses={200: PrioritySerializer, 400: 'Bad Request', 404: 'Not Found'}
+))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(
+    operation_description="Actualiza parcialmente una prioridad específica por ID.",
+    request_body=PrioritySerializer,
+    responses={200: PrioritySerializer, 400: 'Bad Request', 404: 'Not Found'}
+))
+@method_decorator(name='destroy', decorator=swagger_auto_schema(
+    operation_description="Elimina una prioridad específica por ID.",
+    responses={204: 'No Content', 404: 'Not Found'}
+))
 class PriorityViewSet(viewsets.ModelViewSet):
     
     queryset = Priority.objects.all()
@@ -77,7 +132,17 @@ class LoginAPIView(APIView):
             'access': str(refresh.access_token),
         }, status=status.HTTP_200_OK)
     
-
+@swagger_auto_schema(
+    method='get',
+    operation_description="Obtiene una lista de todas las tareas.",
+    responses={200: TaskReadSerializer(many=True)}
+)
+@swagger_auto_schema(
+    method='post',
+    operation_description="Crea una nueva tarea. Acepta múltiples tareas si se envía una lista.",
+    request_body=TaskWriteSerializer(many=True),
+    responses={201: TaskWriteSerializer(many=True), 400: 'Bad Request'}
+)
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def task_list_create(request):
@@ -94,6 +159,28 @@ def task_list_create(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@swagger_auto_schema(
+    method='get',
+    operation_description="Obtiene los detalles de una tarea específica.",
+    responses={200: TaskReadSerializer, 404: 'Not Found'}
+)
+@swagger_auto_schema(
+    method='put',
+    operation_description="Actualiza completamente una tarea específica.",
+    request_body=TaskWriteSerializer,
+    responses={200: TaskWriteSerializer, 400: 'Bad Request', 404: 'Not Found'}
+)
+@swagger_auto_schema(
+    method='patch',
+    operation_description="Actualiza parcialmente una tarea específica.",
+    request_body=TaskWriteSerializer,
+    responses={200: TaskWriteSerializer, 400: 'Bad Request', 404: 'Not Found'}
+)
+@swagger_auto_schema(
+    method='delete',
+    operation_description="Elimina una tarea específica.",
+    responses={204: 'No Content', 404: 'Not Found'}
+)
 @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
 @permission_classes([IsAuthenticatedOrReadOnly, IsOwnerOrAssignedUser])
 def tasks_detail(request, pk, format=None):
@@ -121,7 +208,16 @@ def tasks_detail(request, pk, format=None):
         task.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    
+@swagger_auto_schema(
+    method='get',
+    operation_description="Obtiene una lista de tareas filtradas por estado.",
+    manual_parameters=[
+        openapi.Parameter(
+            'state', openapi.IN_QUERY, description="ID o nombre del estado", type=openapi.TYPE_STRING
+        ),
+    ],
+    responses={200: TaskReadSerializer(many=True), 404: 'Not Found'}
+) 
 @api_view(['GET'])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def task_by_state_list(request):
@@ -142,6 +238,16 @@ def task_by_state_list(request):
     serializer = TaskReadSerializer(tasks, many=True)
     return Response(serializer.data)
 
+@swagger_auto_schema(
+    method='get',
+    operation_description="Obtiene una lista de tareas filtradas por prioridad.",
+    manual_parameters=[
+        openapi.Parameter(
+            'priority', openapi.IN_QUERY, description="ID o nombre de la prioridad", type=openapi.TYPE_STRING
+        ),
+    ],
+    responses={200: TaskReadSerializer(many=True), 404: 'Not Found'}
+) 
 @api_view(['GET'])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def task_by_priority_list(request):
@@ -162,6 +268,19 @@ def task_by_priority_list(request):
     serializer = TaskReadSerializer(tasks, many=True)
     return Response(serializer.data)
 
+@swagger_auto_schema(
+    method='get',
+    operation_description="Obtiene una lista de tareas filtradas por fecha exacta, tareas antes de cierta fecha y tareas posteriores a cierta fecha.",
+    manual_parameters=[
+        openapi.Parameter(
+            'deadline', openapi.IN_QUERY, description="Fecha que se desea buscar (formato YYYY-MM-DD)", type=openapi.TYPE_STRING, default='exact'
+        ),
+        openapi.Parameter(
+            'filter', openapi.IN_QUERY, description="Tipo de filtro (exact, before, after)", type=openapi.TYPE_STRING
+        ),
+    ],
+    responses={200: TaskReadSerializer(many=True), 404: 'Not Found'}
+) 
 @api_view(['GET'])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def task_by_deadline(request):
@@ -185,6 +304,16 @@ def task_by_deadline(request):
     serializer = TaskReadSerializer(tasks, many=True)
     return Response(serializer.data)
 
+@swagger_auto_schema(
+    method='get',
+    operation_description="Obtiene una lista de tareas filtradas por dueño de la tarjeta.",
+    manual_parameters=[
+        openapi.Parameter(
+            'owner', openapi.IN_QUERY, description="ID o nombre de usuario del dueño de la tarjeta", type=openapi.TYPE_STRING
+        ),
+    ],
+    responses={200: TaskReadSerializer(many=True), 404: 'Not Found'}
+)
 @api_view(['GET'])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def task_by_owner(request):
@@ -204,6 +333,16 @@ def task_by_owner(request):
     serializer = TaskReadSerializer(tasks, many=True)
     return Response(serializer.data)
 
+@swagger_auto_schema(
+    method='get',
+    operation_description="Obtiene una lista de tareas filtradas por usuarios asignados.",
+    manual_parameters=[
+        openapi.Parameter(
+            'assigned_users', openapi.IN_QUERY, description="ID o nombre de usuario del usuario asignado", type=openapi.TYPE_STRING
+        ),
+    ],
+    responses={200: TaskReadSerializer(many=True), 404: 'Not Found'}
+)
 @api_view(['GET'])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def task_by_assigned_users(request):
@@ -220,7 +359,15 @@ def task_by_assigned_users(request):
     serializer = TaskReadSerializer(tasks, many=True)
     return Response(serializer.data)
 
-
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_description="Obtiene una lista de todos los comentarios.",
+    responses={200: CommentSerializer(many=True)}
+))
+# @method_decorator(name='create', decorator=swagger_auto_schema(
+#     operation_description="Crea un nuevo comentario.",
+#     request_body=CommentSerializer,
+#     responses={201: CommentSerializer},
+# ))
 class CommentListCreateAPIView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
@@ -229,6 +376,24 @@ class CommentListCreateAPIView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(
+    operation_description="Obtiene un comentario específico por ID.",
+    responses={200: CommentSerializer, 404: 'Not Found'}
+))
+@method_decorator(name='update', decorator=swagger_auto_schema(
+    operation_description="Actualiza un comentario específico por ID.",
+    request_body=CommentSerializer,
+    responses={200: CommentSerializer, 400: 'Bad Request', 404: 'Not Found'}
+))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(
+    operation_description="Actualiza parcialmente un comentario específico por ID.",
+    request_body=CommentSerializer,
+    responses={200: CommentSerializer, 400: 'Bad Request', 404: 'Not Found'}
+))
+@method_decorator(name='destroy', decorator=swagger_auto_schema(
+    operation_description="Elimina un comentario específico por ID.",
+    responses={204: 'No Content', 404: 'Not Found'}
+))
 class CommentDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
