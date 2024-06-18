722e09ec667846ade5a378f21cd0a85c468433fa
Lubin Stiven <lubinstevenr@gmail.com>
2024-06-17 22:31:22 -0500

    se cambio la logica parala construccion de los comentarios, ahora se agregan despues de crear la tarea y el usuario

--- WorkStream/serializers/taskSerializers.py
+++ WorkStream/serializers/taskSerializers.py
@@ -6,30 +6,28 @@ from WorkStream.models import Priority
 from .stateSerializers import StateSerializer
 from .prioritySerializers import PrioritySerializer
 from .customUserSerializers import CustomUserSerializer
-from WorkStream.serializers.commentSerializers import CommentSerializer
 
 class TaskReadSerializer(serializers.ModelSerializer):
     state = StateSerializer()
     priority = PrioritySerializer()
     owner = CustomUserSerializer()
     assigned_users = CustomUserSerializer(many=True)
-    comment = CommentSerializer(many=True, read_only=True)
     class Meta:
         model = Task
         fields = '__all__'
 
 # Serializador para la escritura
 class TaskWriteSerializer(serializers.ModelSerializer):
+
     state = serializers.PrimaryKeyRelatedField(queryset=State.objects.all())
     priority = serializers.PrimaryKeyRelatedField(queryset=Priority.objects.all())
     assigned_users = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all(), many=True)
-    comment = CommentSerializer(many=True, required=False )
+
 
     class Meta:
+        
         model = Task
         exclude = ['owner']
 
-    def create(self, validated_data):
-        user = self.context['request'].user
-        validated_data['owner'] = user
-        return super().create(validated_data)
+   
+        
