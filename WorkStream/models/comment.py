fbbf1b32ce9ee4590ec3699584678073e3fa0e7e
Lubin Stiven <lubinstevenr@gmail.com>
2024-06-19 11:31:53 -0500

    se importo el usuario perspnalizado desde el archivo settings y se quitaron las comillas en la instancia del modelo task

--- WorkStream/models/comment.py
+++ WorkStream/models/comment.py
@@ -1,15 +1,14 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from WorkStream.models import Task
 
-User = get_user_model()
 
 
 class Comment(models.Model):
-    task = models.ForeignKey('Task', related_name='comments', on_delete=models.CASCADE)
+    
+    task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    assigned_users = models.ManyToManyField(User, related_name='assigned_comments')
+    assigned_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='assigned_comments')
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
