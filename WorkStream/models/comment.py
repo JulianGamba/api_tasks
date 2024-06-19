e0cd2740ed6625265a3a4c2d89384223f6ae5c0b
Lubin Stiven <lubinstevenr@gmail.com>
2024-06-19 17:17:26 -0500

    se hicieron algunos cambios en las relaciones de los modelos

--- WorkStream/models/comment.py
+++ WorkStream/models/comment.py
@@ -1,16 +1,16 @@
 from django.db import models
-from django.conf import settings
 from WorkStream.models import Task
-
+from django.conf import settings
 
 
 class Comment(models.Model):
     
     task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    assigned_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='assigned_comments')
+    user = models.ForeignKey('customUser', on_delete=models.CASCADE)
+    assigned_users = models.ManyToManyField('customUser', related_name='assigned_comments')
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text[:20]
+
