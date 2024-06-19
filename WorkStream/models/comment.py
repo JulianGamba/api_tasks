0f73501d3cf797d62a26031dc3370502421cb92b
Lubin Stiven <lubinstevenr@gmail.com>
2024-06-18 21:29:39 -0500

    se cambio el nombre de el campo task_id a task

--- WorkStream/models/comment.py
+++ WorkStream/models/comment.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from WorkStream.models import Task
 
 User = get_user_model()
 
 
 class Comment(models.Model):
-    task_id = models.ForeignKey('Task', related_name='comments', on_delete=models.CASCADE)
+    task = models.ForeignKey('Task', related_name='comments', on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     assigned_users = models.ManyToManyField(User, related_name='assigned_comments')
     text = models.TextField()
