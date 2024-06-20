0b6216c9ff83e2a949b9b0c8288725a7e1e79163
Lubin Stiven <lubinstevenr@gmail.com>
2024-06-19 22:56:25 -0500

    se agrego las importaciones del usuario perosonalizado y se configuro las relaciones

--- WorkStream/models/comment.py
+++ WorkStream/models/comment.py
@@ -1,16 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from WorkStream.models import Task
-from django.conf import settings
+from WorkStream.models import CustomUser
 
+CustomUser = get_user_model()
 
 class Comment(models.Model):
-    
     task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
-    user = models.ForeignKey('customUser', on_delete=models.CASCADE)
-    assigned_users = models.ManyToManyField('customUser', related_name='assigned_comments')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='comments_created')
     text = models.TextField()
+    assigned_users = models.ManyToManyField(CustomUser, related_name='assigned_comments')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text[:20]
-
