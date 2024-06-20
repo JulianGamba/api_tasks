2084ed11856adfea4d2a37b7cb3855d4a106c3e2
Lubin Stiven <lubinstevenr@gmail.com>
2024-06-20 15:51:33 -0500

    se agrego el modelo comments

--- WorkStream/admin.py
+++ WorkStream/admin.py
@@ -3,9 +3,11 @@ from .models.state import State
 from .models.customUser import CustomUser
 from .models.tasks import Task
 from .models.priority import Priority
+from .models.comment import Comment
 
 
 admin.site.register(Task)
 admin.site.register(Priority)
 admin.site.register(State)
 admin.site.register(CustomUser)
+admin.site.register(Comment)
