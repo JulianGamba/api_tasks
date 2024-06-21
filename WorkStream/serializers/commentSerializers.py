b21666cdc6a86623f05fb089802d2c80c0831e57
Lubin Stiven <lubinstevenr@gmail.com>
2024-06-21 15:43:07 -0500

    retiro el campo assigned_Users del modelo comments

--- WorkStream/serializers/commentSerializers.py
+++ WorkStream/serializers/commentSerializers.py
@@ -7,36 +7,11 @@ CustomUser = get_user_model()
 class CommentSerializer(serializers.ModelSerializer):
     task = serializers.PrimaryKeyRelatedField(queryset=Task.objects.all())
     user = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all(), default=serializers.CurrentUserDefault())
-    assigned_users = serializers.PrimaryKeyRelatedField(many=True, queryset=CustomUser.objects.all(), required=False)
 
 
     class Meta:
         model = Comment
-        fields = ['id' , 'user', 'task', 'assigned_users', 'text', 'created_at']
+        fields = ['id' , 'user', 'task', 'text', 'created_at']
         read_only_fields = ['id', 'user', 'created_at']
 
-    def create(self, validated_data):
-        
-        print("Validated Data in create:", validated_data)  # Depuración
-        assigned_users = validated_data.pop('assigned_users', [])
-        comment = Comment.objects.create(**validated_data)
-        comment.assigned_users.set(assigned_users)
-        return comment
-
-    def update(self, instance, validated_data):
-        
-        print("Instance in update:", instance)  # Depuración
-        print("Validated Data in update:", validated_data)  # Depuración
-        assigned_users = validated_data.pop('assigned_users', [])
-        instance = super().update(instance, validated_data)
-        instance.assigned_users.set(assigned_users)
-        return instance
-from rest_framework import serializers
-from WorkStream.models.comment import Comment
-
-class CommentSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')  # Solo lectura, se establece automáticamente
-
-    class Meta:
-        model = Comment
-        fields = ['id', 'user', 'text', 'created_at']
+  
