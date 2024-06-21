473c57ccb216f8d15a90ec604af77ea8ac7e7eb2
Julian Gamba <gahe2003@gmail.com>
2024-06-21 15:28:24 -0500

    Se eliminan usuarios asignados de los comentarios.

--- WorkStream/models/comment.py
+++ WorkStream/models/comment.py
@@ -9,7 +9,6 @@ class Comment(models.Model):
     task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='comments_created')
     text = models.TextField()
-    assigned_users = models.ManyToManyField(CustomUser, related_name='assigned_comments')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- WorkStream/permissions.py
+++ WorkStream/permissions.py
@@ -49,23 +49,3 @@ class IsCommentOwner(permissions.BasePermission):
 
         # Write permissions are only allowed to the owner of the comment
         return obj.user == request.user
-
-
-class IsOwnerOrAssignedUser(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Write permissions are only allowed to the owner or assigned users
-        return obj.owner == request.user or request.user in obj.assigned_users.all()
-
-
-class IsCommentOwner(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Write permissions are only allowed to the owner of the comment
-        return obj.user == request.user
