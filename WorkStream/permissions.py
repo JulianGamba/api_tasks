251ba59c4dcab891af1b2b265f52edf44e46b42f
Lubin Stiven <lubinstevenr@gmail.com>
2024-06-26 15:17:50 -0500

    se resuelven conflictos

--- WorkStream/permissions.py
+++ WorkStream/permissions.py
@@ -1,6 +1,5 @@
 from rest_framework import permissions
 
-
 class IsAuthenticatedOrReadOnly(permissions.BasePermission):
     """
     Custom permission to only allow authenticated users to edit it.
@@ -17,29 +16,19 @@ class IsAuthenticatedOrReadOnly(permissions.BasePermission):
         # Write permissions are only allowed to authenticated users.
         return request.user and request.user.is_authenticated
 
-
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to edit it.
-    Read permissions are allowed to any request,
-    so we'll always allow GET, HEAD or OPTIONS requests.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
-
-
 class IsOwnerOrAssignedUser(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request
         if request.method in permissions.SAFE_METHODS:
             return True
+        
+        # Debugging statements
+        print(f"Request user: {request.user}")
+        print(f"Task owner: {obj.owner}")
+        print(f"Assigned users: {obj.assigned_users.all()}")
 
         # Write permissions are only allowed to the owner or assigned users
         return obj.owner == request.user or request.user in obj.assigned_users.all()
-
 
 class IsCommentOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
