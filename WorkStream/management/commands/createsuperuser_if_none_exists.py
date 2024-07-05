1d87c6de450c1681e25cc6d4337d16d2b8514197
Lubin Stiven <lubinstevenr@gmail.com>
2024-07-05 11:12:53 -0500

    se creo el archivo management para la creacion automatica del superUser d Django con respecto a la documentacion

---
+++ WorkStream/management/commands/createsuperuser_if_none_exists.py
@@ -0,0 +1,18 @@
+# myapp/management/commands/createsuperuser_if_none_exists.py
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = 'Create a superuser if none exists'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        if not User.objects.filter(is_superuser=True).exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='admin@example.com',
+                password='adminpass'
+            )
+            self.stdout.write(self.style.SUCCESS('Superuser created.'))
+        else:
+            self.stdout.write(self.style.SUCCESS('Superuser already exists.'))
