57c9294eb7cb21c6ce74d2f23cf89a8f896f00fc
Lubin Stiven <lubinstevenr@gmail.com>
2024-07-05 11:14:50 -0500

    se registro el comado personalizado en el archivo apps.py

--- WorkStream/apps.py
+++ WorkStream/apps.py
@@ -7,3 +7,15 @@ class WorkStreamConfig(AppConfig):
 
     def ready(self):
         import WorkStream.signals
+
+    def ready(self):
+        try:
+            from django.core.management import load_command_class
+
+            import WorkStream.signals  # Importar señales si las tienes
+
+            load_command_class(
+                "myapp", "createsuperuser_if_none_exists"
+            )  # Registrar el comando personalizado
+        except ImportError:
+            pass
